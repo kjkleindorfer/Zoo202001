7c071605f80057e6d24b38694fa9db7bd7e8944b
DanielBMeeker <54420579+DanielBMeeker@users.noreply.github.com>
2019-08-27 20:08:49 -0500

    Pikachu-I choose you

---
+++ src/model/ElectricMouse.java
@@ -0,0 +1,48 @@
+package model;
+
+//Daniel Meeker
+public class ElectricMouse {
+	private String name;
+	private double height;
+	private double weight;
+
+	public ElectricMouse() {
+		super();
+	}
+
+	public ElectricMouse(String name, double height, double weight) {
+		super();
+		this.name = name;
+		this.height = height;
+		this.weight = weight;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	public String speak() {
+		return "Pika Pika!";
+	}
+
+}
