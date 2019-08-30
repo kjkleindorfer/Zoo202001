a05bd62dffcf3d8533c48ce64e2cbd0580ef2735
JenniLJ <48532943+JenniLJ@users.noreply.github.com>
2019-08-29 23:35:35 -0500

    Added Groundhog

--- src/model/Groundhog.java
+++ src/model/Groundhog.java
@@ -2,9 +2,36 @@ package model;
 
 public class Groundhog {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private String name;
+	private boolean shadow;
+	private String birthdate;
+	
+	
+	public Groundhog() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public boolean isShadow() {
+		return shadow;
+	}
+	public void setShadow(boolean shadow) {
+		this.shadow = shadow;
+	}
+	public String getBirthdate() {
+		return birthdate;
+	}
+	public void setBirthdate(String birthdate) {
+		this.birthdate = birthdate;
 	}
 
+	public String speak() {
+		return "nope";
+	}
 }
